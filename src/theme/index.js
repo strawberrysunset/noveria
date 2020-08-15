ea38a34d97d2ba2e4ab520d7ebaa20d4c59eca23
AvalancheLab <avalanchelab@protonmail.com>
2020-08-15 16:34:50 +0100

    Update exports.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,15 +1,34 @@
 export const colors = {
     blue : {
-        100 : '#fff',
-        200 : 'red'
+        100 : '#3E4349',
+        200 : '#8295AA',
+        300 : '#D3DEEA',
+        400 : '#E3E8EF',
+    },
+    green : {
+        100 : '#66BF84'
+    },
+    neutral : {
+        100: '#D1D4D9',
+        200 : '#DFE0E1',
+        300 : '#F4F7F9'
+    },
+    red : {
+        100 : '#E19696'
     }
 }
 
 export const typeScale = {
-    
+    h1 : '3rem',
+    h2 : '2rem',
+    h3 : '1.6rem',
+    h4 : '1.25rem',
+    body : '1rem',
+    small : '0.8rem',
+    caption : '0.625rem'
 }
 
+
 export const fonts = {
-    default : 'Roboto',
-    header : 'Hind'
+    default : 'Overpass'
 }
