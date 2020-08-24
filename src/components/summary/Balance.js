8edd4bf033e12b4002b5880e12340c323fdef636
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-24 18:03:40 +0100

    Add account switching buttons.

--- src/components/summary/Balance.js
+++ src/components/summary/Balance.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-// import {usePortfolio} from '../../stores'
-import {colors, typeScale} from '../../theme'
-import { MdKeyboardArrowDown as ArrowIcon } from 'react-icons/md'
+import { useAPI } from '../../stores'
+import { colors, typeScale } from '../../theme'
+import {
+    MdKeyboardArrowDown as ArrowIcon,
+    MdSubscriptions as RefreshingIcon,
+} from 'react-icons/md'
 
 const Wrapper = styled.div`
     display: flex;
@@ -12,20 +15,18 @@ const Wrapper = styled.div`
 `
 
 const Arrow = styled(ArrowIcon)`
-    transform: ${props => {
-        if (props.right) return `rotate(-90deg)`
-        if (props.left) return `rotate(90deg)`
+    transform: ${(props) => {
+        if (props.direction === 'right') return `rotate(-90deg)`
+        if (props.direction === 'left') return `rotate(90deg)`
     }};
 `
-
 
 const Main = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    color : ${colors.neutral[300]};
-    
+    color: ${colors.neutral[300]};
 `
 
 const Account = styled.p`
@@ -42,22 +43,25 @@ const Total = styled.div`
 
 const AltTotal = styled.div`
     font-size: ${typeScale.bodySmall};
-    color : ${colors.neutral[100]};
+    color: ${colors.neutral[100]};
 `
 
 export const Balance = () => {
-
-    // const {activeAccount: {name, total}} = usePortfolio();
+    const fetching = useAPI((state) => state.fetching)
 
     return (
         <Wrapper>
-            <Arrow left size="1.5rem" color={colors.neutral[300]}/>
+            <Arrow direction="left" size="1.5rem" color={colors.neutral[300]} />
             <Main>
                 <Account>Savings</Account>
-                <Total>$2,130,120</Total>
+                <Total>{fetching.isActive ? 'LOADING' : '$2,130,120'}</Total>
                 <AltTotal>(0.25 BTC)</AltTotal>
             </Main>
-            <Arrow right size="1.5rem" color={colors.neutral[300]}/>
+            <Arrow
+                direction="right"
+                size="1.5rem"
+                color={colors.neutral[300]}
+            />
         </Wrapper>
     )
 }
