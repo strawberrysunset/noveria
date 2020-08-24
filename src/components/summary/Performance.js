ffeb84511f9e5b6fd15578f814d68655669fc400
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-24 18:05:56 +0100

    Add code formatting changes.

--- src/components/summary/Performance.js
+++ src/components/summary/Performance.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Card} from '../common'
+import { Card } from '../common'
 
-const Wrapper = styled.div`
-
-`
+const Wrapper = styled.div``
 
 export const Performance = () => {
-    return (
-        <Wrapper>
-
-        </Wrapper>
-    )
+    return <Wrapper></Wrapper>
 }
