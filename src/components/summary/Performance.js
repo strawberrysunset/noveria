e9dce41e8025a3dac7ad365b82486f102aff4939
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-27 11:13:35 +0100

    Delete files.

--- src/components/summary/Performance.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Card } from '../common'
-
-const Wrapper = styled.div``
-
-export const Performance = () => {
-    return <Wrapper></Wrapper>
-}
