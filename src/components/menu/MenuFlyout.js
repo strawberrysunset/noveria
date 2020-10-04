8c0239c951a8d3356b6034e3a93512f5c44f8a93
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-04 17:31:53 +0100

    Remove animation and update styles.

--- src/components/menu/MenuFlyout.js
+++ src/components/menu/MenuFlyout.js
@@ -25,70 +25,45 @@ const Wrapper = styled(motion.div)`
 `
 
 const MenuListWrapper = styled.ul`
-  padding-top: 1rem;
+  /* padding-top: 1rem; */
+  border-top: 0.1rem solid ${props => props.theme.colors.neutral[1200]}; 
   display: flex;
   flex-direction: column;
-  overflow-y: auto;
   position: relative;
   flex-grow: 1;
   border-bottom: 1px solid ${props => props.theme.colors.neutral[300]}; 
   /* background: ${props => props.theme.colors.neutral[100]}; */
 `
 
-// const Overlay = styled.div`
-//   display: contents;
-//   position: fixed;
-//   bottom: 0;
-//   width: 100%;
-//   height: 4rem;
-//   background: linear-gradient(0deg, ${props => props.theme.colors.neutral[100]}, rgba(0, 0, 0, 0));
-// `
-
-// const FadeOverlay = styled(motion.div)`
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   left: 0;
-//   top:0;
-//   bottom:0;
-//   right: 0;
-//   background-color: ${props => props.theme.colors.neutral[100]};
-// `
-
 export const MenuFlyout = ({ ...props }) => {
 
-  const [menu] = useMenu()
-
-  const currentList = menu.currentList()
-
-  console.log(currentList)
+  const {isOpen, items} = useMenu()
 
   const variants = {
     visible: i => ({ 
-      x: 0,
       opacity: 1,
-      transition: {
-        delay: i * 0.05,
-        duration: 0.05,
-        ease: 'easeIn'
-      }
+      // transition: {
+      //   duration: 0.03,
+      //   ease: 'easeIn'
+      // }
     }),
     hidden: { 
       opacity: 0,
-      x: -320
     },
   }
 
+  const bitems = items()
+
   return (
     <AnimatePresence>
-      {menu.showing && (
+      {isOpen && (
         <Wrapper initial={{ x: -320 }} animate={{ x: 0 }} exit={{ x: -320 }} transition={{ ease: "easeInOut", duration: 0.2}} {...props}>
           <MenuListWrapper>
-            {currentList.map((item, idx) => <MenuItem 
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-              custom={idx}
+            {bitems.map((item, idx) => <MenuItem 
+              // initial="hidden"
+              // animate="visible"
+              // variants={variants}
+              // custom={idx}
               key={generateUniqueID()}
               {...item}
             />)}
