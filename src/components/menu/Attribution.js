2477ccbf10972507054f1bec02817da8d8ea1fa8
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:37:04 +0100

    Update styling.

--- src/components/menu/Attribution.js
+++ src/components/menu/Attribution.js
@@ -1,48 +1,35 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import {CoinGeckoLogo, NoveriaLogo} from '../../assets'
+import {CoinGeckoLogo, CoinTelegraphLogo} from '../../assets'
 
 const Wrapper = styled.div`
-  /* padding: 2rem 0; */
+  padding: 2rem 0;
   display: grid;
   align-items: center;
   grid-gap: 0.5rem;
-  /* grid-auto-flow: column; */
   justify-content: center;
   font-weight: 600;
+  border-top: 1px solid ${props => props.theme.colors.neutral[300]}; 
 `
 
-const Text = styled.div`
-  font-size: 0.8rem;
+const LogosWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `
 
-const Logo = styled(CoinGeckoLogo)`
+const CoinGecko = styled(CoinGeckoLogo)`
   height: 2.25rem;
   margin-top: 0.3rem;
+  border-right: 1px solid ${props => props.theme.colors.neutral[800]};
+  padding-right: 0.5rem;
 `
-
-const LogoWrapper = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  border: 1px solid ${props => props.theme.colors.neutral[300]};
-  /* flex-direction: column; */
-  grid-gap: 1px;
-  background-color: ${props => props.theme.colors.neutral[300]};
-  align-items: stretch;
-  > * {
-    padding: 0.25rem 1rem;
-    background-color: ${props => props.theme.colors.neutral[100]};
-  }
-`
-
-const NoveriaWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const CoinTelegraph = styled(CoinTelegraphLogo)`
+  margin-left: 1rem;
 `
 
 const Poweredby = styled.p`
   margin: 0 auto;
+  margin-bottom: 0.5rem;
   text-transform: uppercase;
   font-size: 0.8rem;
   letter-spacing: 0.1em;
@@ -53,15 +40,11 @@ export const Attribution = ({ ...rest }) => {
 
   return (
     <Wrapper {...rest}>
-      {/* <NoveriaWrapper>
-        <NoveriaLogo/>
-        
-      </NoveriaWrapper> */}
       <Poweredby>Noveria is Powered by</Poweredby>
-      <LogoWrapper>
-        <Logo />
-        <Text>CoinTelegraph</Text>
-      </LogoWrapper>
+      <LogosWrapper>
+        <CoinGecko/>
+        <CoinTelegraph/>
+      </LogosWrapper>
     </Wrapper>
   )
 }
