8b9c06b44ecdba36525cd2a0d7354669b5c490dc
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:33:56 +0100

    Add offset parameter.

--- src/components/animators/InfiniteSpin.js
+++ src/components/animators/InfiniteSpin.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 
-export const InfiniteSpin = ({children, duration}) => {
+export const InfiniteSpin = ({children, duration = 0.5, offset = 0}) => {
   return (
     <motion.div 
-      initial={{ rotate: '0deg' }}
-      animate={{ rotate: '360deg' }}
-      transition={{ duration, repeat: Infinity, ease: 'linear' }}
+      initial={{ rotate: `${0 + offset}deg` }}
+      animate={{ rotate: `${360 + offset}deg` }}
+      transition={{ duration, repeat: Infinity, ease: 'easeInOut' }}
     >
       {children}
     </motion.div>
