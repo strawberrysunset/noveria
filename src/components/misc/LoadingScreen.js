2a3b94f16d3112e6005042a704a7beeb2247f369
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:40:31 +0100

    Update styling.

--- src/components/misc/LoadingScreen.js
+++ src/components/misc/LoadingScreen.js
@@ -5,6 +5,7 @@ import { LoadingBar } from './LoadingBar'
 import {useTheme, useAPI} from '../../context'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useIsFetching } from 'react-query'
+import {Spinner} from '../common'
 
 const Wrapper = styled(motion.div)`
   position: absolute;
@@ -22,6 +23,28 @@ const Wrapper = styled(motion.div)`
   flex-direction: column;
 `
 
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`
+
+const Logo = styled(NoveriaLogo)`
+  
+`
+
+const Spin =  styled(Spinner)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 3rem;
+  height: 3rem;
+  z-index: 10000;
+`
+
 export const LoadingScreen = ({ ...rest }) => {
 
   const isFetching = useIsFetching()
@@ -36,13 +59,17 @@ export const LoadingScreen = ({ ...rest }) => {
   return (
     <AnimatePresence>
       {!loaded && isFetching && (
+        
         <Wrapper
           {...rest}
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
-        >
-          <NoveriaLogo />
+        > 
+        {/* <LogoWrapper> */}
+          {/* <Spin/> */}
+          <Logo/>
+        {/* </LogoWrapper> */}
         </Wrapper>
       )}
     </AnimatePresence>
