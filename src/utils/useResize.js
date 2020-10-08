92b343585db18ed7fed0409ef529fade40b5d0b2
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:55:06 +0100

    Move function outside of useEffect.

--- src/utils/useResize.js
+++ src/utils/useResize.js
@@ -3,13 +3,18 @@ import React from "react";
 export const useResize = (myRef) => {
   const [width, setWidth] = React.useState(0);
   const [height, setHeight] = React.useState(0);
- 
+
+  const handleResize = () => {
+    setWidth(myRef.current.offsetWidth);
+    setHeight(myRef.current.offsetHeight);
+  };
+
   React.useEffect(() => {
-    const handleResize = () => {
-      setWidth(myRef.current.offsetWidth);
-      setHeight(myRef.current.offsetHeight);
-    };
- 
+    handleResize()
+  }, [])
+
+  React.useEffect(() => {
+    
     window.addEventListener("resize", handleResize);
  
     return () => {
