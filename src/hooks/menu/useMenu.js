614dfa4f0f3356be358a1e3f0f492b6bfa3c2918
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:53:08 +0100

    Change naming covention.

--- src/hooks/menu/useMenu.js
+++ src/hooks/menu/useMenu.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import {BaseList} from '../../components/menu/lists'
+import {useBaseList} from '../../components/menu/lists'
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'toggle_menu': {
       return {...state, isOpen: !state.isOpen }
     }
-    case 'set_items' : {
-      const items = action.items
-      const itemsHistory = [...state.itemsHistory, action.items]
-      return {...state, items, itemsHistory }
+    case 'set_list' : {
+      const listHistory = [...state.listHistory, action.list]
+      return {...state, useCurrentList: action.list, listHistory }
     }
     case 'go_back' : {
 
-      // Current becomes previous items in itemsHistory.
-      const items = state.itemsHistory[state.itemsHistory.length - 2]
+      // Current becomes previous list in listHistory.
+      const useCurrentList = state.listHistory[state.listHistory.length - 2]
 
-      // Overly complex removal of last items although necessarily avoids direct state mutation.
-      const itemsHistory = state.itemsHistory.filter((items, i) => {
-        if (i !== (state.itemsHistory.length - 1)) {
-          return items
+      // Overly complex removal of last list although necessarily avoids direct state mutation.
+      const listHistory = state.listHistory.filter((list, i) => {
+        if (i !== (state.listHistory.length - 1)) {
+          return list
         }
       })      
-      return {...state, items, itemsHistory }
+      return {...state, useCurrentList, listHistory }
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -33,8 +32,8 @@ export const reducer = (state, action) => {
 export const useMenu = () => {
   const [menu, updateMenu] = React.useReducer(reducer, {
     isOpen: false,
-    items: BaseList,
-    itemsHistory: [BaseList]
+    useCurrentList: useBaseList,
+    listHistory: [useBaseList]
   })
   return {...menu, updateMenu}
 }
