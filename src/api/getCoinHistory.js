4f0b7a92c226629ccf04ffb529956c2cdfdd870e
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-08 04:31:56 +0100

    Extract prices field.

--- src/api/getCoinHistory.js
+++ src/api/getCoinHistory.js
@@ -1,5 +1,6 @@
 import {jsonFetch} from 'utilities'
 
 export const getCoinHistory = async ({id, currency, days}) => {
-  return await jsonFetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`)
+  const {prices} = await jsonFetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`)
+  return prices
 }
