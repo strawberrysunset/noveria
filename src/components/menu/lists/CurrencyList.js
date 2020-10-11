8b6c4ae9e5ce4612bd3016d4f098c09e06fa88fa
AvalancheLab <avalanchelab@protonmail.com>
2020-10-11 05:27:49 +0100

    Add temporary hardcoding while fixing issue.

--- src/components/menu/lists/CurrencyList.js
+++ src/components/menu/lists/CurrencyList.js
@@ -4,19 +4,83 @@ import {useSupportedCurrencies} from '../../../hooks/api'
 import {MdCheck} from 'react-icons/md'
 import {ListItem} from './ListItem'
 
+
+// const supportedCurrencies = [
+//   "btc",
+//   "eth",
+//   "ltc",
+//   "bch",
+//   "bnb",
+//   "eos",
+//   "xrp",
+//   "xlm",
+//   "link",
+//   "dot",
+//   "yfi",
+//   "usd",
+//   "aed",
+//   "ars",
+//   "aud",
+//   "bdt",
+//   "bhd",
+//   "bmd",
+//   "brl",
+//   "cad",
+//   "chf",
+//   "clp",
+//   "cny",
+//   "czk",
+//   "dkk",
+//   "eur",
+//   "gbp",
+//   "hkd",
+//   "huf",
+//   "idr",
+//   "ils",
+//   "inr",
+//   "jpy",
+//   "krw",
+//   "kwd",
+//   "lkr",
+//   "mmk",
+//   "mxn",
+//   "myr",
+//   "nok",
+//   "nzd",
+//   "php",
+//   "pkr",
+//   "pln",
+//   "rub",
+//   "sar",
+//   "sek",
+//   "sgd",
+//   "thb",
+//   "try",
+//   "twd",
+//   "uah",
+//   "vef",
+//   "vnd",
+//   "zar",
+//   "xdr",
+//   "xag",
+//   "xau"
+// ]
 export const useCurrencyList = () => {
 
   const {currency: currentCurrency, updateSettings} = useSettings()
-  const {supportedCurrencies} = useSupportedCurrencies()
+  const {updateMenu} = useMenu()
+  const {supportedCurrencies}= useSupportedCurrencies()
 
-  return !supportedCurrencies ? [] : supportedCurrencies.reduce((list, currency) => {
-    console.log(typeof list)
+  return supportedCurrencies.reduce((list, currency, idx) => {
+    // Always put selected currency at top of currency list.
     if (currency === currentCurrency) {
-      list.unshift(<ListItem left={currentCurrency.toUpperCase()} right={<MdCheck/>}/>)
+      list.unshift(<ListItem key={idx} left={currentCurrency.toUpperCase()} right={<MdCheck/>}/>)
     } else {
-      list.push(<ListItem left={currency.toUpperCase()} onClick={() => updateSettings({type: 'set_currency', currency})}/>)
+      list.push(<ListItem key={idx} left={currency.toUpperCase()} onClick={() => {
+        updateSettings({type: 'set_currency', currency})
+        updateMenu({type: 'go_back'})
+      }}/>)
     }
     return list
   }, [])
-
 }
