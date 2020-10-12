63a5959e2bc8a1d3452e20a1b1a6e0cb033b59f6
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:40:17 +0100

    Add page prefetching.

--- src/hooks/api/useCoinData.js
+++ src/hooks/api/useCoinData.js
@@ -1,11 +1,21 @@
 import {getCoinData} from '../../api'
+import React from 'react'
 import {usePaginatedQuery, queryCache} from 'react-query'
 import {useSettings} from '../../context'
+import { useEffect } from 'react'
 export const useCoinData = ({coins = undefined, page, perPage} = {}) => {
   const {currency} = useSettings()
   const key = ['coin', page, perPage, coins, currency]
-  // queryCache.prefetchQuery(key, getCoinData({currency, coins, page: page + 1, perPage}))
-  const {data, ...asyncInfo} = usePaginatedQuery(key, async () => getCoinData({currency, coins, page, perPage}))
+
+  // Preload next page
+  React.useEffect(() => {
+    queryCache.prefetchQuery(key, getCoinData({currency, coins, page: page + 1, perPage}))
+  }, [page])
+  
+  const {data, ...asyncInfo} = usePaginatedQuery(key, async () => {
+    return getCoinData({currency, coins, page, perPage})
+  })
+  
   return {coinData: data || [], ...asyncInfo}
   // sort out pagination here
 }
