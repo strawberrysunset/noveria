e63ea516c61ee4b052bfce0ef7b470a9f6837fac
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:39:35 +0100

    Remove global header stats.

--- src/components/pages/markets/CardHeaderItems.js
+++ src/components/pages/markets/CardHeaderItems.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled, {css} from 'styled-components/macro'
 import { useGlobalData } from '../../../hooks/api'
-import {Percentage} from '../../common'
+import {Input} from '../../common'
 import {useFormatPrice} from '../../../hooks/common'
 
 const Wrapper = styled.div`
@@ -37,6 +37,7 @@ const Right = styled.ul`
   display: grid;
   grid-auto-flow: column;
   grid-gap: 1.5rem;
+  margin-left: auto;
   
 `
 
@@ -55,23 +56,14 @@ const NavButton = styled.p`
 
 export const CardHeaderItems = ({page, setPage}) => {
 
-  const {isLoading, globalData} = useGlobalData()
-  const {formatPrice} = useFormatPrice()
-
-  return React.useMemo(() => (
+  return (
     <Wrapper>
-      <Left>
-        <Item>Assets: {isLoading ? 'Loading...' : globalData.activeCoins}</Item>
-        <Item>Markets: {isLoading ? 'Loading...' : globalData.markets}</Item>
-        <Item>Total Market Cap: {isLoading ? 'Loading...' : formatPrice(globalData.marketCap)}</Item>
-        <Item>Total Volume: {isLoading ? 'Loading...' : formatPrice(globalData.volume)}</Item>
-        <Item>BTC Dominance: {isLoading ? 'Loading...' : <Percentage>{ globalData.dominance['btc']}</Percentage>}</Item>
-      </Left>
       <Right>
+        {/* <NavButton enabled={page > 1} onClick={()=> setPage(1)}>First</NavButton> */}
         <NavButton enabled={page > 1} onClick={()=> setPage(page - 1)}>Previous</NavButton>
         <Item css={`font-weight: bold;`}>{page}</Item>
-        <NavButton enabled={page < 4} onClick={()=> setPage(page + 1)}>Next</NavButton>
+        <NavButton enabled={page < 20} onClick={()=> setPage(page + 1)}>Next</NavButton>
       </Right>
     </Wrapper> 
-  ), [globalData])
+  )
 }
