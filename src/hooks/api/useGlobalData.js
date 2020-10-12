320562125027543d7a31b0bc01818205443e4526
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:40:47 +0100

    Change syntax.

--- src/hooks/api/useGlobalData.js
+++ src/hooks/api/useGlobalData.js
@@ -1,8 +1,11 @@
 import {useQuery} from 'react-query'
 import {getGlobalData} from '../../api'
 import { useSettings } from '../../context'
+
 export const useGlobalData = () => {
   const {currency} = useSettings()
-  const {data, isLoading, ...asyncInfo} = useQuery(['globalData', currency], () => getGlobalData({currency}))
-  return {globalData: data, isLoading, ...asyncInfo}
+  const {data, ...asyncInfo} = useQuery(['globalData', currency], async () => {
+    return await getGlobalData({currency})
+  })
+  return {globalData: data, ...asyncInfo}
 }
