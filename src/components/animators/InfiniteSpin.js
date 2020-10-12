ec7eb2b77d20b035af89b3555e1d656e9f0fc640
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:30:29 +0100

    Pass props.

--- src/components/animators/InfiniteSpin.js
+++ src/components/animators/InfiniteSpin.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import {css} from 'styled-components'
 
-export const InfiniteSpin = ({children, duration = 0.5, offset = 0}) => {
+export const InfiniteSpin = ({children, duration = 0.5, offset = 0, ...rest}) => {
   return (
-    <motion.div 
+    <motion.div  
       initial={{ rotate: `${0 + offset}deg` }}
       animate={{ rotate: `${360 + offset}deg` }}
       transition={{ duration, repeat: Infinity, ease: 'easeInOut' }}
+      {...rest}
     >
       {children}
     </motion.div>
