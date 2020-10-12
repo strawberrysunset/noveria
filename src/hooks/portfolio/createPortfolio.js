25a7ef306eaa213842d0d4dfa275b3f65da0cb86
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:43:04 +0100

    Reorganize code.

--- src/hooks/portfolio/createPortfolio.js
+++ src/hooks/portfolio/createPortfolio.js
@@ -1,7 +1,11 @@
 import {getCoinData} from '../../api'
+import React from 'react'
+import { getPortfolioHistory } from './getPortfolioHistory'
 
-export const createPortfolio = async ({assets: staleAssets, currency, exchangeRates}) => {
+export const createPortfolio = async ({assets: staleAssets, currency, exchangeRates, historyDays}) => {
   
+
+  // const history = await getPortfolioHistory({assets, currency, days: historyDays})
   const data = await getCoinData({coinIDs: staleAssets.map(coin => coin.id), currency})
 
   const assets = staleAssets.map(asset => {
@@ -9,7 +13,6 @@ export const createPortfolio = async ({assets: staleAssets, currency, exchangeRa
     const price = assetData.spotPrice.value * asset.amount;
     return {
       ...asset, 
-      ...assetData,
       price,
       change: {
         '24h' : {
@@ -17,8 +20,9 @@ export const createPortfolio = async ({assets: staleAssets, currency, exchangeRa
           percentage: assetData.spotPrice.change['24h'].percentage
         }
       },
-      // Ternary ensures BTC asset precision error from conversion.
-      priceBTC: (assetData.symbol == 'btc') ? asset.amount : (price / exchangeRates[currency].value)
+      // If asset is btc, price is the amount, no neeed to convert.
+      priceBTC: (assetData.symbol == 'btc') ? asset.amount : (price / exchangeRates[currency].value),
+      ...assetData,
     }
   })
 
@@ -28,8 +32,6 @@ export const createPortfolio = async ({assets: staleAssets, currency, exchangeRa
     return total
   }, [0, 0])
 
-  
-
   const weightedAssets = assets.map(asset => {
     return {
       ...asset,
@@ -37,12 +39,18 @@ export const createPortfolio = async ({assets: staleAssets, currency, exchangeRa
     }
   })
 
-  const change  = {
-    '24h': {
-      value : weightedAssets.reduce((total, asset) => total += asset.change['24h'].value, 0),
-      percentage : weightedAssets.reduce((total, asset) => total += asset.change['24h'].percentage * asset.weight, 0)
-    }
-  } 
+  
 
-  return {assets: weightedAssets, total, totalBTC, change}
+  return {
+    assets: weightedAssets, 
+    total, 
+    totalBTC, 
+    isEmpty: weightedAssets.length === 0, 
+    change : {
+      '24h': {
+        value : weightedAssets.reduce((total, asset) => total += asset.change['24h'].value, 0),
+        percentage : weightedAssets.reduce((total, asset) => total += asset.change['24h'].percentage * asset.weight, 0)
+      }
+    } 
+  }
 }
