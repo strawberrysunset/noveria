9ab44a68c8b03ebc827d473ef9e07fca6230839b
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:30:16 +0100

    Update styles and add popUp.

--- src/components/index.js
+++ src/components/index.js
@@ -10,6 +10,7 @@ import {PopUp} from './misc'
 import {Error} from './pages/Error'
 import {getSupportedCurrencies, getNewsFeed} from '../api'
 import {queryCache} from 'react-query'
+import {useNotification, usePortfolio} from '../context'
 
 
 const GlobalStyling = createGlobalStyle`
@@ -18,6 +19,7 @@ const GlobalStyling = createGlobalStyle`
     list-style: none;
     vertical-align: middle;
   }
+  
   html {
     font-family: 'Overpass', sans-serif;
     
@@ -62,73 +64,73 @@ const SiteWrapper = styled.div`
   max-height: 100vh;
 
   position: relative;
-  display: grid;
-  grid-template-rows: auto minmax(0, 1fr);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `
 
 const HeaderSticky = styled(Header)`
-  position: sticky;
-  top: 0;
 `
 
 const Main = styled.main`
   position: relative;
   display: flex;
   overflow: hidden;
-  flex-direction: column;
   align-items: stretch;
-  justify-content: stretch;
-  margin-bottom: 3rem;
-  @media (min-width: 48rem) {
-    flex-direction: row;
+  flex-grow: 1;
+  ${props => props.theme.isMobile && css`
+    flex-direction: column-reverse;
     margin-bottom: 0rem;
-  };
+  `}
 `
 
 const NavSticky = styled(Nav)`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 3rem;
+  display: grid;
+  grid-auto-flow: row;
+  height: 100%;
+  width: 4rem;
   border-top: 1px solid ${(props) => props.theme.colors.neutral[200]};
-  @media (min-width: 48rem) {
-    grid-auto-flow: row;
+  ${props => props.theme.isMobile && css`
+    grid-auto-flow: column;
     border-top: 0;
     border-right: 1px solid ${(props) => props.theme.colors.neutral[200]};;
     position: relative;
-    min-width: 4rem;
-    max-width: 4rem;
-    height: 100%;
-  } ;
+    min-height: 3.5rem;
+    max-height: 3.5rem;
+    width: 100%;
+  `}
 `
 
 const StyledSwitch = styled(Switch)`
   flex-grow: 1;
-  overflow-x: hidden;
-  overflow-y: auto;
 `
 
 export const App = () => {
+
+  const {showPopUp, popUpContent, updateNotification} = useNotification()
+  const portfolio = usePortfolio()
 
   React.useEffect(() => {
     queryCache.prefetchQuery('newsFeed', getNewsFeed)
   }, [])
 
+  React.useEffect(() => {
+    if (portfolio.assets.length > 0) updateNotification({type: 'hidePopUp'})
+  }, [portfolio])
+
   return (
     <>
       <GlobalStyling />
       <LoadingScreen css={`z-index: 999;`}/>
-      <PopUp css={`z-index: 998;`} />
+      <PopUp showing={showPopUp} handleClose={() => updateNotification({type: 'hidePopUp'})} content={popUpContent}/>
       <SiteWrapper>
-        <HeaderSticky css={`z-index: 4;`}/>
+        <HeaderSticky/>
         <Main>
-          <MenuFlyout css={`z-index: 3;`}/>
-          <NavSticky css={`z-index: 2;`}/>
-          <StyledSwitch>
+          <MenuFlyout css={`z-index: 1;`}/>
+          <NavSticky css="grid-area: nav;"/>
+          <StyledSwitch css="grid-area: page;">
             {pages.map((page, idx) => (
-              <Route exact={page.path === '/home'} key={idx} path={page.path} component={page.component} />
+              <Route exact={page.path === '/'} key={idx} path={page.path} component={page.component} />
             ))}
             <Route component={Error}></Route>
           </StyledSwitch>
