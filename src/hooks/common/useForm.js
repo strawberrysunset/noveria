2f7e65f9e0261fdf55eb730180e5bde54040c51d
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:41:33 +0100

    Add pause and manual set methods.

--- src/hooks/common/useForm.js
+++ src/hooks/common/useForm.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React from "react"
 
 export const useForm = ({initialValues, onSubmit}) => {
+
   const [values, setValues] = React.useState(initialValues);
   const [error, setError] = React.useState('')
   const [isSubmitting, setIsSubmitting] = React.useState(false)
+  const [submitPause, setSubmitPause] = React.useState(1000)
+
+  const handleChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  const submit = async event => {
+    event.preventDefault()
+    if (error) setError('')
+    setIsSubmitting(true)
+    await onSubmit(values)
+    .catch(error => {
+      setError(error.message)
+      setIsSubmitting(false)
+    })
+    setTimeout(() => setIsSubmitting(false), submitPause) // Prevent spam.
+  }
+
+  const manualSet = (obj) => {
+    setValues({...values, ...obj});
+  }
+
   return {
     values,
     error,
     isSubmitting,
-    handleChange: event => {
-      setValues({
-        ...values,
-        [event.target.name]: event.target.value
-      });
-    },
-    submit: async (event) => {
-      event.preventDefault()
-      if (error) setError('')
-      setIsSubmitting(true)
-      await onSubmit(values)
-      .catch(error => setError(error.message))
-      setIsSubmitting(false)
-    }
+    manualSet,
+    handleChange,
+    submit,
+    setSubmitPause
   };
 };
