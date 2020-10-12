00fc3c1f3673c079df39b57fe9e6b60320db7f48
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:43:57 +0100

    Remove reducer methods and change fields.

--- src/hooks/portfolio/usePortfolio.js
+++ src/hooks/portfolio/usePortfolio.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {useQuery, useQueryCache} from 'react-query'
-import {generateUniqueID} from 'utilities'
 import {useSettings} from '../../context'
 import {createPortfolio} from './createPortfolio'
-import {useExchangeRates, useSupportedCurrencies} from '../../hooks/api'
+import {useExchangeRates} from '../../hooks/api'
 import {getPortfolioHistory} from './getPortfolioHistory'
-import {useLocalStorageSync} from '../misc'
+import {useLocalStorageReducer} from '../misc'
+import { generateUniqueID } from 'utilities'
 
 export const reducer = (assets, action) => {
   switch (action.type) {
@@ -13,13 +13,10 @@ export const reducer = (assets, action) => {
       return [...assets, action.asset]
     }
     case 'remove_asset': {
-      return assets.filter((asset) => action.id !== asset.uniqueID)
+      return assets.filter((asset) => asset.uniqueID !== action.uniqueID)
     }
     case 'remove_all_assets': {
       return []
-    }
-    case 'set_assets' : {
-      return action.assets
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -27,20 +24,19 @@ export const reducer = (assets, action) => {
   }
 }
 
-export const middleware = (dispatch, action) => {
-  
+export const middleware = async (dispatch, action) => {
   switch (action.type) {
     case 'create_asset' : {
-      if (!action.amount || action.amount.match(/^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/) === null) {
+      if (!action.amount || String(action.amount).match(/^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/) === null) {
         throw new Error('Specified amount is not valid.')
       }
       if (!action.id) {
         throw new Error('Asset id is undefined.')
       }
       const asset = {
-        uniqueID: generateUniqueID(),
         id: action.id,
-        amount: parseFloat(action.amount),
+        amount: Number(action.amount),
+        uniqueID: generateUniqueID()
       }
       return dispatch({type: 'create_asset', asset})
     }
@@ -52,53 +48,46 @@ export const middleware = (dispatch, action) => {
 
 const defaultPortfolio = {
   assets : [],
-  total : {
-    default: 0,
-    btc: 0
-  },
+  total : 0,
+  totalBTC: 0,
   change: {
     '24h' : {
       value: 0,
       percentage: 0
     }
-  }
+  },
+  isEmpty: true
 }
 
 export const usePortfolio = () => {
 
-  const [assets, dispatch] = React.useReducer(reducer, [])
-  const updatePortfolio = (action) => middleware(dispatch, action)
+  const [assets, dispatch] = useLocalStorageReducer('noveria-p', reducer, [])
   
+  const updatePortfolio = (action) => middleware(dispatch, action)
+
   const {currency} = useSettings()
-  const {supportedCurrencies} = useSupportedCurrencies() 
   const {exchangeRates} = useExchangeRates()
+  const [historyDays, setHistoryDays] = React.useState()
   const queryCache = useQueryCache()
 
-  useLocalStorageSync(assets, 'noveria-portfolio', (localAssets) => {
-      updatePortfolio({type: 'set_assets', assets: localAssets})
-  }, [assets])
-
-  const {data: portfolio, ...asyncInfo} = useQuery(['portfolio-data', supportedCurrencies, assets, currency], async () => {
+  const {data, ...asyncInfo} = useQuery(['portfolio-data', assets, currency, exchangeRates], () => {
     return createPortfolio({assets, currency, exchangeRates})
-  })
-
-  const usePortfolioHistory = ({days}) => {
-    const {data: history, ...asyncInfo} = useQuery(['portfolio-history', portfolio?.assets, currency, days], () => {
-      return getPortfolioHistory({assets: portfolio?.assets, currency, days})
-    })
-    return {history, ...asyncInfo}
-  }
-
-  const refresh = () => {
-    queryCache.refetchQueries(['portfolio-data', supportedCurrencies, assets, currency])
-  }
+  }, {keepPreviousData: true, initialData: defaultPortfolio})
 
   return {
-    ...(portfolio), 
+    ...data, 
     ...asyncInfo, 
-    usePortfolioHistory, 
+    setHistoryDays, 
     updatePortfolio,
-    refresh
+    refresh: () => queryCache.refetchQueries(['portfolio-data', assets, currency, exchangeRates]),
+    rawAssets: assets
   }
 }
+
+// const usePortfolioHistory = ({days}) => {
+//   const {data: history, ...asyncInfo} = useQuery(['portfolio-history', portfolio.assets, currency, days], () => {
+//     return 
+//   })
+//   return {history, ...asyncInfo}
+// }
 
