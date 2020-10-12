f4072ffb30775d1916269a12a8bbb07f2699876c
AvalancheLab <avalanchelab@protonmail.com>
2020-10-12 14:43:23 +0100

    Add default assets value and remove comment.

--- src/hooks/portfolio/getPortfolioHistory.js
+++ src/hooks/portfolio/getPortfolioHistory.js
@@ -2,7 +2,6 @@ import {getCoinHistory} from '../../api'
 
 const sumHistories = (histories) => {
   return histories.reduce((total, history) => {
-    console.log({history})
     history.forEach((el, idx) => {
       total[idx][0] = el[0]
       total[idx][1] += el[1]
@@ -11,7 +10,8 @@ const sumHistories = (histories) => {
   }, Array(histories[0].length).fill([0, 0]))
 }
 
-export async function getPortfolioHistory ({assets = [], currency, days}) {
+export async function getPortfolioHistory ({assets, currency, days}) {
+  if (!assets) return []
   const histories = await Promise.all(assets.map(async asset => {
     return await getCoinHistory({id: asset.id, currency, days})
   }, []))
