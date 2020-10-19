f40ccb84745bff3b3d107e7a37013aac38b3ab08
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-19 14:08:17 +0100

    Add card states, i.e. spinner and error msg.

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-
+import {Spinner} from '../common'
 
 const Wrapper = styled.div`
   position: relative;
@@ -9,6 +9,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
+  
 `
 
 const Label = styled.div`
@@ -23,6 +24,7 @@ const LabelWrapper = styled.div`
   grid-auto-flow: column;
   grid-gap: 0.5rem;
   margin-right: 2.5rem;
+  color: ${(props) => props.theme.colors.neutral[1400]};
 `
 
 const Header = styled.div`
@@ -30,16 +32,30 @@ const Header = styled.div`
   min-height: 3.5rem;
   max-height: 3.5rem;
   padding: 0 1.5rem;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-  border-top: 1px solid rgba(255, 255, 255, 0.1);
-  /* background: linear-gradient(90deg, ${props => props.theme.colors.neutral[400]}, 5%, ${props => props.theme.colors.neutral[200]}); */
-  background: ${props => props.theme.colors.neutral[200]};
+  border-bottom: 1px solid ${props => props.theme.colors.neutral[800]};
+  /* background: linear-gradient(90deg, ${props => props.theme.colors.neutral[400]}, ${props => props.theme.colors.neutral[100]}); */
+  background: ${props => props.theme.colors.neutral[400]};
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
 
-export const Card = ({ icon: Icon, label, children, items, ...rest }) => {
+const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Message = styled.p`
+  color: ${props => props.theme.colors.neutral[1200]};
+  ${props => props.error && css`
+    color: props.colors.red[100];
+  `}
+`
+
+export const Card = ({ icon: Icon, label, children, items, message, error, loading=false, ...rest }) => {
   return (
     <Wrapper {...rest}>
       <Header>
@@ -49,7 +65,15 @@ export const Card = ({ icon: Icon, label, children, items, ...rest }) => {
         </LabelWrapper>
         {items}
       </Header>
-      {children}
+      <ContentWrapper>
+        {getPlaceholderState() || children}
+      </ContentWrapper> 
     </Wrapper>
   )
+
+  function getPlaceholderState () {
+    if (loading) return <Spinner/>
+    if (error) return <Message error="true">{error}</Message>
+    if (message) return <Message>{message}</Message>
+  }
 }
