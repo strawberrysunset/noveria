108abf1bcec39556b4103f54628033f69327fe7f
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-19 14:06:31 +0100

    Delete files.

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-export * from './NoveriaLogo'
-export * from './DarkModeButton'
-export * from './CoinGeckoLogo'
-export * from './CoinTelegraphLogo'
--- src/hooks/theme/themes/index.js
+++
@@ -1,9 +0,0 @@
-import dark from './dark'
-import light from './light' 
-
-export const themes = {
-  dark, 
-  light
-}
-
-export * from './common'
