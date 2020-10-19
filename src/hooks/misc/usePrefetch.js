cb472f7b71cc7fdaa00b6701275014302606efe3
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-19 15:33:02 +0100

    Delete files.

--- src/hooks/misc/usePrefetch.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {queryCache} from 'react-query'
-import {getSupportedCurrencies, getNewsFeed, getCoinData} from '../../api'
-
-export const usePrefetch = () => {
-  
-}
