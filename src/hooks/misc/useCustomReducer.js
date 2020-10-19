a6752bc003a3c4f77e5fc135c1537967421a799f
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-19 15:33:16 +0100

    Create custom reducer.

--- src/hooks/misc/useCustomReducer.js
+++ src/hooks/misc/useCustomReducer.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import {getLocalState} from './getLocalState'
 
-export const useCustomReducer = ({initialState, init, reducer, middleware, saveToLocalStorage}) => {
+export const useCustomReducer = ({reducerArgs, middleware, saveToLocalStorage}) => {
 
-  const [state, dispatch] = React.useReducer(reducer, initialState, init)
+  const initialState = saveToLocalStorage.isEnabled && (getLocalState({key: saveToLocalStorage.key}) || reducerArgs[1])
+
+  const [state, dispatch] = React.useReducer(
+    reducerArgs[0],
+    initialState || undefined, 
+    reducerArgs[2]
+  )
 
   const modifiedDispatch = (action) => middleware(state, dispatch, action)
 
   // If saveToLocalStorage is enabled, save the state.
   React.useEffect(() => {
-    if (saveToLocalStorage) {
+    if (saveToLocalStorage.isEnabled) {
       localStorage.setItem(saveToLocalStorage.key, JSON.stringify(state))
     }
   }, [saveToLocalStorage, state])
