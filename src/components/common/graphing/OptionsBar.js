7616a007e2e025463a853ec7be0dec933293ccd7
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-19 14:14:01 +0100

    Replace options with children and update styles.

--- src/components/common/graphing/OptionsBar.js
+++ src/components/common/graphing/OptionsBar.js
@@ -12,27 +12,22 @@ const Wrapper = styled.div`
 const Option = styled.p`
   padding: 0.25rem 0.5rem;
   color: ${(props) => {
-    return props.selected ? props.theme.colors.neutral[1600] : props.theme.colors.neutral[800]
+    return props.selected ? props.theme.colors.neutral[1600] : props.theme.colors.neutral[1200]
   }};
   :hover {
     cursor: pointer;
-    color: ${(props) => props.theme.colors.neutral[1200]};
+    color: ${(props) => props.theme.colors.neutral[1400]};
   }
   transition: 0.1s ease;
 `
 
-export const OptionsBar = ({ options, ...rest }) => {
+export const OptionsBar = ({ children, ...rest }) => {
 
   const [selected, setSelected] = useLocalStorageState('history-options', 0)
 
   return (
     <Wrapper {...rest}>
-      {options.map(({value, action}, idx) => (
-        <Option key={idx} selected={selected === idx} onClick={() => {
-          setSelected(idx)
-          action()
-        }}>{value}</Option>
-      ))}
+      {children}
   </Wrapper>
   )
 }
