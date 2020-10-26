88536fcf02a1b9a6085278c53c28a2d8a4cdefa0
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:14:20 +0000

    Delete files.

--- src/hooks/misc/getLocalState.js
+++
@@ -1,8 +0,0 @@
-export const getLocalState = ({key, fallback}) => {
-    const localState = localStorage.getItem(key)
-    if (localState) {
-        const localStateParsed = JSON.parse(localState)
-        return localStateParsed
-    }
-    return fallback
-}
--- src/hooks/notification/createPopUp.js
+++
@@ -1,6 +0,0 @@
-import { PopUp } from '../../components/misc'
-import React from 'react'
-
-export const createPopUp = ({handleClose, content}) => {
-  return <PopUp content={content} handleClose={handleClose}/>
-}
