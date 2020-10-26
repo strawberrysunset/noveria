76733c5904d88d0963e96654f4e28ea8d3595980
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:40:33 +0000

    Add mobile popUp asset creation & update styles.

--- src/components/pages/portfolio/index.js
+++ src/components/pages/portfolio/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import { AssetTable } from './AssetTable'
 import { AssetManager } from './AssetManager'
+import {AiFillPlusSquare} from 'react-icons/ai'
+import {PopUp} from '../../common'
+import {useTheme} from '../../../context'
 
 const Wrapper = styled.div`
+  position: relative;
   background: ${(props) => props.theme.colors.neutral[300]};
   display: grid;
   grid-gap: 1px;
@@ -16,17 +20,51 @@ const Wrapper = styled.div`
   ${props => props.theme.isMobile && css`
     grid-auto-flow: row;
     grid-template-columns: 100%;
-    grid-template-rows: auto 1fr;
+    grid-template-rows: 1fr;
     /* height: 100%; */
     overflow-y: auto;
   `}
 `
 
+const AddAsset = styled(AiFillPlusSquare)`
+  position: absolute;
+  top: 1rem;
+  left: calc(50% - 1rem);
+  ${props => !props.theme.isMobile && css`
+    display: none;
+  `};
+  height: 2rem;
+  width: 2rem;
+  :hover {
+    cursor: pointer;
+  }
+  color: ${props => props.theme.colors.neutral[1400]};
+`
+const StyledAssetManager = styled(AssetManager)`
+  /* ${props => props.theme.isMobile && css`
+    display: none;
+  `} */
+  overflow-y: auto;
+`
+
+const StyledPopUp = styled(PopUp)`
+  ${props => !props.theme.isMobile && css`
+    display: none;
+  `};
+`
+
 export const Portfolio = () => {
+  const theme = useTheme()
+  const [showing, setShowing] = React.useState(false);
   return (
     <Wrapper>
-      <AssetManager/>
-      <AssetTable css="overflow-y: auto;"/>
+      {theme.isMobile 
+      ? <StyledPopUp showClose={false} showing={showing} render={() => {
+        return <StyledAssetManager handleClose={() => setShowing(false)}/>
+      }}/>
+      :<StyledAssetManager/>
+      }
+      <AssetTable showPopUp={() => setShowing(true)} css="overflow-y: auto;"/>
     </Wrapper>
   )
 }
