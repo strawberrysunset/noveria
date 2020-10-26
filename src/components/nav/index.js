a7672f6b388f2078de6a130f809e193d9b73d653
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:25:37 +0000

    Make nav items local & remove highlight.

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,8 +2,32 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import { useLocation } from 'react-router-dom'
 import { NavItem } from './NavItem'
-import { pages } from '../pages'
-import { usePortfolio } from '../../hooks/portfolio'
+import {
+  MdHome as HomeIcon,
+} from 'react-icons/md'
+import {FaGlobeAmericas as MarketsIcon} from 'react-icons/fa'
+import {BiNews as NewsIcon} from 'react-icons/bi'
+import {RiBriefcase4Fill as PortfolioIcon} from 'react-icons/ri'
+
+const pages = [
+  {
+    path: '/',
+    icon: HomeIcon,
+  },
+  {
+    path: '/portfolio',
+    icon: PortfolioIcon,
+  },
+  {
+    path: '/markets',
+    icon: MarketsIcon,
+  },
+  {
+    path: '/news',
+    icon: NewsIcon,
+  },
+]
+
 
 
 const Wrapper = styled.div`
@@ -13,19 +37,19 @@ const Wrapper = styled.div`
   grid-auto-columns: 1fr;
   grid-gap: 1px;
   ${props => props.theme.isMobile && css`
-    border-top: 1px solid white;
+    border-top: 1px solid ${(props) => props.theme.colors.neutral[1200]};
     border-right: none;
   `}
+  
   border-right: 1px solid ${(props) => props.theme.colors.neutral[800]};
 `
 
 export const Nav = ({ ...rest }) => {
   const { pathname } = useLocation()
-  const { assets } = usePortfolio()
 
   const NavItems = pages.map(({ path, icon }, idx) => {
     return (
-      <NavItem highlight={(assets.length === 0) && path === '/portfolio'} key={idx} path={path} icon={icon} active={pathname === path} />
+      <NavItem key={idx} path={path} icon={icon} active={pathname === path} />
     )
   })
 
