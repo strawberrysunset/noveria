24c23e59a1e68745afe219bd7235d61862bb907f
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:45:00 +0000

    Remove popUp.

--- src/hooks/notification/useNotification.js
+++ src/hooks/notification/useNotification.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import {Greeting} from '../../components/pages/home/Greeting'
 
 export const reducer = (state, action) => {
-
   switch (action.type) {
     case 'set_message' : {
       return {...state, message : action.message}
-    }
-    case 'set_popUp_content' : {
-      return {...state, popUpContent: action.popUpContent}
-    }
-    case 'set_showPopUp': {
-      return {...state, showPopUp: action.showPopUp}
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -19,32 +11,9 @@ export const reducer = (state, action) => {
   }
 }
 
-export const middleware = (state, dispatch, action) => {
-  switch(action.type) {
-    case 'showPopUp': {
-      dispatch({ type: 'set_popUp_content', popUpContent: action.popUpContent })
-      return dispatch({ type: 'set_showPopUp', showPopUp: true })
-    }
-    case 'hidePopUp': {
-      dispatch({ type: 'set_showPopUp', showPopUp: false })
-      return dispatch({ type: 'set_popUp_content', popUpContent: null })
-    }
-    case 'set_message' : {
-      return dispatch({type: 'set_message', message: action.message})
-    }
-    default: {
-      dispatch(action)
-    }
-  }
-}
-
 export const useNotification = () => {
-  const [notification, dispatch] = React.useReducer(reducer, {
-    popUpContent : <Greeting/>,
-    showPopUp: true,
-    message : ''
+  const [notification, updateNotification] = React.useReducer(reducer, {
+    message : 'Welcome to Noveria'
   })
-
-  const updateNotification = (action) => middleware(notification, dispatch, action)
   return {...notification, updateNotification}
 }
