7fcc4ed11079473b789d0f935459a522884c63fa
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:45:46 +0000

    Add memoized middleware function caller.

--- src/hooks/misc/useCustomReducer.js
+++ src/hooks/misc/useCustomReducer.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import {getLocalState} from './getLocalState'
+import {getLocalStorageState} from './getLocalStorageState'
 
-export const useCustomReducer = ({reducerArgs, middleware, saveToLocalStorage}) => {
-
-  const initialState = saveToLocalStorage.isEnabled && (getLocalState({key: saveToLocalStorage.key}) || reducerArgs[1])
+export const useCustomReducer = ({reducerArgs = [], middleware = [], saveToLocalStorage = {}}) => {
 
   const [state, dispatch] = React.useReducer(
     reducerArgs[0],
-    initialState || undefined, 
+    saveToLocalStorage.isEnabled ? getLocalStorageState({key: saveToLocalStorage.key, fallback: reducerArgs[1]}) : reducerArgs[1],
     reducerArgs[2]
   )
 
-  const modifiedDispatch = (action) => middleware(state, dispatch, action)
-
   // If saveToLocalStorage is enabled, save the state.
   React.useEffect(() => {
-    if (saveToLocalStorage.isEnabled) {
-      localStorage.setItem(saveToLocalStorage.key, JSON.stringify(state))
-    }
-  }, [saveToLocalStorage, state])
+    localStorage.setItem(saveToLocalStorage.key, JSON.stringify(state))
+  }, [state])
 
-  return [state, modifiedDispatch]
+  // Call each middleware function.
+  const interceptedDispatch = React.useCallback(async action => {
+    if (!middleware) return dispatch(action)
+    // Middleware could be async so assume all are and await each.
+    await Promise.all(middleware.map(async fn => {
+      await fn(state, dispatch, action)
+    }))
+  }, [middleware, state, dispatch])
+
+  return [state, interceptedDispatch]
 }
