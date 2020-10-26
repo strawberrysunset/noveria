bb3cf87a269fa408a880bd9a860166aa2d9732e1
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:28:41 +0000

    Update styles, move text outside fn & add button.

--- src/components/pages/home/Greeting.js
+++ src/components/pages/home/Greeting.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import { NoveriaLogo } from '../../assets'
+import {Button} from '../../common'
 
 const Wrapper = styled.div`
   text-align: center;
@@ -17,17 +18,20 @@ const Subtitle = styled.p`
   line-height: 1.7;
   font-size: ${(props) => props.theme.typeScale.body};
   color: ${props => props.theme.colors.neutral[1300]};
+  margin-bottom: 0.5rem;
+  margin-top: 0.5rem;
 `
 
-const title = 'Hi there and welcome to Noveria'
-const subtitle = 'Noveria is an account-less cryptocurrency portfolio tracker that runs in your browser. It provides you with statistics obout your portfolio, market information and more. Enjoy!'
+const title = 'Welcome to Noveria'
+const subtitle = 'Noveria is an account-less cryptocurrency portfolio tracker that runs in your browser. It provides you with statistics about your portfolio, market information and more. Enjoy!'
 
-export const Greeting = ({ ...rest }) => {
+export const Greeting = ({ handleClose, ...rest }) => {
   return (
     <Wrapper {...rest}>
       <NoveriaLogo />
       <Title>{title}</Title>
       <Subtitle>{subtitle}</Subtitle>
+      <Button onClick={handleClose}>Dismiss</Button>
     </Wrapper>
   )
 }
