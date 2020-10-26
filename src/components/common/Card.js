4054bd0be6f04e98458661326b1ef1620676ee3e
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:19:37 +0000

    Update styles and add placeholder.

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components/macro'
 import {Spinner} from '../common'
+import {transparentize} from 'polished'
 
 const Wrapper = styled.div`
   position: relative;
@@ -8,8 +9,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
-  
+  max-height: 100%;
 `
 
 const Label = styled.div`
@@ -31,21 +31,25 @@ const Header = styled.div`
   width: 100%;
   min-height: 3.5rem;
   max-height: 3.5rem;
-  padding: 0 1.5rem;
-  border-bottom: 1px solid ${props => props.theme.colors.neutral[800]};
+  padding: 0.25rem 1.5rem;
+  border-top: 0.5rem solid ${props => props.theme.colors.neutral[1100]};
+  border-bottom: 1px solid ${props => props.theme.colors.neutral[1100]};
   /* background: linear-gradient(90deg, ${props => props.theme.colors.neutral[400]}, ${props => props.theme.colors.neutral[100]}); */
-  background: ${props => props.theme.colors.neutral[400]};
+  background: ${props => props.theme.colors.neutral[800]};
   display: flex;
+
   align-items: center;
   justify-content: space-between;
 `
 
 const ContentWrapper = styled.div`
+  position: relative;
   width: 100%;
-  height: 100%;
+  height: calc(100% - 3.5rem);
+  flex-grow: 1;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: stretch;
+  justify-content: stretch;
 `
 
 const Message = styled.p`
@@ -53,9 +57,37 @@ const Message = styled.p`
   ${props => props.error && css`
     color: props.colors.red[100];
   `}
+  text-align: center;
 `
 
-export const Card = ({ icon: Icon, label, children, items, message, error, loading=false, ...rest }) => {
+const PlaceHolderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  ${props => props.transparentBackground 
+  ? css`
+    background-color: ${props => transparentize(0.1, props.theme.colors.neutral[100])};
+  `
+  : css`
+    background-color: ${props => props.theme.colors.neutral[100]};
+  `}
+  
+  z-index: 2;
+  padding: 2.5rem;
+  
+`
+
+export const Card = ({ children, icon: Icon, transparentBackground=false, label, render, items, message, isLoading, error, ...rest }) => {
+
+  const placeholderState = getPlaceholderState();
+
   return (
     <Wrapper {...rest}>
       <Header>
@@ -66,13 +98,14 @@ export const Card = ({ icon: Icon, label, children, items, message, error, loadi
         {items}
       </Header>
       <ContentWrapper>
-        {getPlaceholderState() || children}
+        {placeholderState && <PlaceHolderWrapper transparentBackground={transparentBackground}>{placeholderState}</PlaceHolderWrapper>}
+        {children}
       </ContentWrapper> 
     </Wrapper>
   )
 
   function getPlaceholderState () {
-    if (loading) return <Spinner/>
+    if (isLoading) return <Spinner/>
     if (error) return <Message error="true">{error}</Message>
     if (message) return <Message>{message}</Message>
   }
