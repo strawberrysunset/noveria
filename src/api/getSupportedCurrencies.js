b54c4f48b24e6a7f12cf0ba1595be31117e0fbae
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:16:45 +0000

    Sort array before returning.

--- src/api/getSupportedCurrencies.js
+++ src/api/getSupportedCurrencies.js
@@ -1,5 +1,6 @@
 import { jsonFetch } from 'utilities'
 
-export const getSupportedCurrencies = () => {
-  return jsonFetch(`https://api.coingecko.com/api/v3/simple/supported_vs_currencies`)
+export const getSupportedCurrencies = async () => {
+  const data = await jsonFetch(`https://api.coingecko.com/api/v3/simple/supported_vs_currencies`)
+  return data.sort()
 }
