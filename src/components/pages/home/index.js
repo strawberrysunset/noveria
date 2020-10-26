f2a4c418bcfe875c1aad00f16c130f3791873a2d
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:29:47 +0000

    Use popUp locally, update styles & add visit flag.

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,51 +1,57 @@
 import React from 'react'
 import styled, {css} from 'styled-components/macro'
 import { Balance } from './Balance'
-import { History } from './History'
-import { Breakdown } from './Breakdown'
-import { PulldownRebound } from '../../animators'
-import { MarketBar } from './MarketBar'
-import {useNotification} from '../../../hooks/notification'
-import {PopUp} from '../../misc'
-
+import { Statistics } from './Statistics'
+import { Markets } from './Markets'
+import {Greeting} from './Greeting'
+import {PopUp} from '../../common'
+import {useSettings} from '../../../context'
 
 const Wrapper = styled.div`
   height: 100%;
   width: 100%;
   display: grid;
   grid-gap: 1px;
-  background: ${props => props.theme.colors.neutral[800]};
-  grid-template-columns: 1fr 2fr;
-  grid-template-rows: 45% 55%;
+  background: ${props => props.theme.colors.neutral[1100]};
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 42% 58%;
   grid-template-areas:
-      /* 'markets markets' */
       'balance markets'
-      'balance history';
+      'statistics markets';
 
   ${props => props.theme.isMobile && css`
     overflow: hidden; 
     overflow-y: auto;
     grid-gap: 1px;
+    grid-template-rows: auto 1fr;
     grid-template-columns: 1fr;
     grid-template-areas:
       'balance'
-      'markets'
-      'history';
+      'statistics';
+  `}
+`
+
+const StyledMarkets = styled(Markets)`
+  ${props => props.theme.isMobile && css`
+    display: none;
   `}
 `
 
 export const Home = () => {
 
+  const {firstVisit, updateSettings} = useSettings()
+
   return (
     <Wrapper>
-      {/* <MarketBar
-        css={`
-          grid-area: markets;
-        `}
-      /> */}
+      {firstVisit && <PopUp showClose={false} render={({setShowing}) => (
+        <Greeting handleClose={() => {
+          setShowing(false)
+          updateSettings({type: 'set_firstVisit', firstVisit: false})
+        }}/>
+      )}/>}
       <Balance css="grid-area: balance;"/>
-      <Breakdown css="grid-area: markets;"/>
-      <History css="grid-area: history;"/>
+      <StyledMarkets css="grid-area: markets;"/>
+      <Statistics css="grid-area: statistics;"/>
     </Wrapper>
   )
 }
