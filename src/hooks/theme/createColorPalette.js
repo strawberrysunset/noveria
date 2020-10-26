add87db3f759e0044727ba708e5bb3789096d8be
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-26 23:41:54 +0000

    Handle undefined targetHue & adjust saturation.

--- src/hooks/theme/createColorPalette.js
+++ src/hooks/theme/createColorPalette.js
@@ -4,12 +4,13 @@ export function createColorPalette (neutralShades, colors, targetHue) {
   palette['neutral'] = {}
   for (const num in neutralShades) {
     const shadeHSL = convert.hex.hsl(neutralShades[num])
-    palette['neutral'][num] = '#' + convert.hsl.hex([targetHue, targetHue ? 15 : 0, shadeHSL[2]])
+    palette['neutral'][num] = '#' + convert.hsl.hex([targetHue, targetHue ? 25 : 0, shadeHSL[2]])
   }
   for (const color in colors) {
     const hsl = convert.hex.hsl(colors[color])
     palette[color] = {}
-    palette[color][100] = '#' + convert.hsl.hex([hsl[0] + (0.1 * (targetHue - hsl[0])), hsl[1], hsl[2]])
+    const hue = targetHue ? hsl[0] + (0.1 * (targetHue - hsl[0])) : hsl[0];
+    palette[color][100] = '#' + convert.hsl.hex([hue, hsl[1], hsl[2]])
   }
   return palette;
 }
