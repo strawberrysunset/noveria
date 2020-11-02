c66c32cd9d3ae65f114834b12645d4a429d55074
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-02 22:59:33 +0000

    Remove wrapper.

--- src/components/assets/NoveriaLogo.js
+++ src/components/assets/NoveriaLogo.js
@@ -1,34 +1,13 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import { 
-  MdFilterHdr as Logo 
-} from 'react-icons/md'
-// import {GiAbstract060 as Logo} from 'react-icons/gi'
-import {
-  // GiDiplodocus as Logo,
-  // GiEclipse as Logo,
-  // GiDrinkMe as Logo,
-} from 'react-icons/gi'
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 2rem;
-  height: 2rem;
-  
-`
+import { MdFilterHdr as Logo } from 'react-icons/md'
 
 const Noveria = styled(Logo)`
-  height: 100%;
-  width: 100%;
+  width: 2rem;
+  height: 2rem;
   color: ${props => props.theme.colors.neutral[1500]};
 `
 
 export const NoveriaLogo = ({...rest}) => {
-  return (
-    <Wrapper {...rest}>
-      <Noveria />
-    </Wrapper>
-  )
+  return <Noveria {...rest}/>
 }
