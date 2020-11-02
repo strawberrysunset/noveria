58f08b1719a825323c08bfe020d97400489e0e5c
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-02 14:54:32 +0000

    Remove comment and page parameters.

--- src/hooks/api/useCoinData.js
+++ src/hooks/api/useCoinData.js
@@ -2,9 +2,13 @@ import {getCoinData} from '../../api'
 import {useQuery} from 'react-query'
 import {useSettings} from '../../context'
 
-export const useCoinData = ({page, perPage, config} = {}) => {
+export const useCoinData = ({config} = {}) => {
+
   const {currency} = useSettings()
+
+  // Fetch coinData.
   return useQuery(['coinData', currency], async () => {
     return await getCoinData({currency})
   }, {initialData: [], staleTime: 30, ...config})
+
 }
