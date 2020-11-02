d6ccbb3dd0d6f68d55e17bbd13ac5364be62e9c0
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-02 14:51:30 +0000

    Update styles, add stat cards.

--- src/components/pages/home/Balance.js
+++ src/components/pages/home/Balance.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import styled, {css} from 'styled-components/macro'
 import {Card, Price, IndicatorColor} from '../../common'
-import {MdAccountBalanceWallet as Icon, MdArrowDropUp as Arrow, MdRefresh } from 'react-icons/md'
+import {MdAccountBalanceWallet as Icon, MdArrowDropUp as Arrow } from 'react-icons/md'
 import {motion} from 'framer-motion'
-import {usePortfolio} from '../../../context'
+import {usePortfolio, useTheme} from '../../../context'
 import {useFormatPrice} from '../../../hooks/misc'
 import {transparentize} from 'polished'
-import { useQueryCache } from 'react-query'
-import {BiUpArrow} from 'react-icons/bi'
 import {formatPercentage} from 'utilities'
+import {StatCard} from './StatCard'
+import {mix, lighten, darken} from 'polished'
+import {getAssetStatistics} from './getAssetStatistics'
 
+
+const StyledCard = styled(Card)`
+  flex-grow: 1;
+`
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
-  height: 100%;
   width: 100%;
-  padding: 1.5rem;
-  background-color: ${props => props.theme.colors.neutral[400]};
+  height: 100%;
+  min-width: 100%;
+  background-color: ${props => props.theme.colors.neutral[600]};
+  ${props => props.theme.isMobile && css`
+    overflow-y: auto;
+  `}
 `
 
 const Total = styled(motion.p)`
@@ -43,6 +49,7 @@ const IndicatorValue = styled.p`
   font-weight: bold;
   margin-left: -0.5rem;
   margin-right: 1rem;
+  
 `
 
 const IndicatorArrow = styled(Arrow)`
@@ -67,48 +74,129 @@ const Color = styled(IndicatorColor)`
   display: flex;
   align-items: center;
   margin-bottom: -1rem;
+  text-shadow: 0rem 0rem 1.5rem ${props => transparentize(0.5, props.theme.colors.green[100])};
 `
 
 const Label = styled.div`
+  height: min-content;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
-  padding: 1rem 0;
+  flex-grow: 1;
+  padding: 3rem 0;
+  box-shadow: 0rem 0rem 2rem ${props => {
+    const toMix = props.positive ? props.theme.colors.green[100] : props.theme.colors.red[100]
+    const color = mix(0.5, toMix, props.theme.colors.neutral[400])
+    return props.theme.darkMode ? color : darken(0.05, color);
+  }} inset;
+  border: 1px solid ${props => props.theme.colors.neutral[1200]};
+  background: ${props => props.theme.colors.neutral[900]};
+  ${props => props.theme.isMobile && css`
+    border: none;
+  `}
 `
 
-const OptionItem = styled.div`
-  ${props => props.selected ? css`
-    color: ${props.theme.colors.neutral[1500]};
-  ` : css`color: ${props.theme.colors.neutral[1200]};`
+const StatsWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem;
+  :after{
+    content:"";
+    display:block;
+    height:0.5rem; /* Which is the padding of div.container */
   }
-  :hover {
-    color: ${props => props.theme.colors.neutral[1600]};
-    cursor: pointer;
-  }
-  transition: 0.1s ease;
+  overflow-y: auto;
+  display: grid;
+  grid-gap: 1.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(min-content, 1fr));
+  ${props => props.theme.isMobile && css`
+    display: none;
+  `}
 `
 
-const priceChangePeriods = ['1H', '24H', '7D', '30D'];
+const Value = styled.p``
 
 export const Balance = ({ ...rest }) => {
 
-  const {total, totalBTC, change, isFetching, isEmpty} = usePortfolio()
+  const {assets, total, totalBTC, change, isFetching, isEmpty} = usePortfolio()
   const {formatPrice} = useFormatPrice()
-
+  const stats = getAssetStatistics({assets})
+  const theme = useTheme()
 
   return (
-    <Card label="Balance (24H)" isLoading={!isEmpty && isFetching} icon={Icon} {...rest}>
+    <StyledCard hideHeader={theme.isMobile} label="Portfolio (24H)" icon={Icon} {...rest}>
       <Wrapper>
-          <Label>
+        <Label isLoading={isFetching} positive={change['24h'].percentage >= 0}>
             <Color value={change['24h'].percentage}>
               <IndicatorArrow value={change['24h'].percentage}/>
               <IndicatorValue>{formatPercentage(change['24h'].percentage)}</IndicatorValue>
             </Color>
             <Total>{formatPrice(total)}</Total>
             <AltTotal>(<Price currency="btc">{totalBTC}</Price>)</AltTotal>
-          </Label>
+        </Label>
+        {/* <StatsOverflow> */}
+        <StatsWrapper>
+          <StatCard 
+            disabled={isEmpty}
+            label="Best Performing Asset (24H)" 
+            asset={stats.bestPerformingAsset} 
+            value={(stats.bestPerformingAsset &&
+                <IndicatorColor isPositive={stats.bestPerformingAsset.change['24h'].percentage > 0}>
+                  <Value>{formatPercentage(stats.bestPerformingAsset.change['24h'].percentage)}</Value>
+                </IndicatorColor>
+            )}
+          />
+          <StatCard 
+            disabled={isEmpty}
+            label="Worst Performing Asset (24H)" 
+            asset={stats.worstPerformingAsset} 
+            value={(stats.worstPerformingAsset &&
+              <IndicatorColor isPositive={stats.worstPerformingAsset.change['24h'].percentage > 0}>
+                <Value>{formatPercentage(stats.worstPerformingAsset.change['24h'].percentage)}</Value>
+              </IndicatorColor>
+            )}
+          />
+          <StatCard 
+            disabled={isEmpty}
+            label="Highest Value Asset" 
+            asset={stats.highestValueAsset} 
+            value={(stats.highestValueAsset &&
+              <IndicatorColor isPositive={stats.highestValueAsset.price > 0}>
+                <Value>{formatPrice(stats.highestValueAsset.price)}</Value>
+              </IndicatorColor>
+            )}
+          />
+          <StatCard 
+            disabled={isEmpty}
+            label="Lowest Value Asset" 
+            asset={stats.lowestValueAsset} 
+            value={(stats.lowestValueAsset &&
+              <IndicatorColor isPositive={stats.lowestValueAsset.price > 0}>
+                <Value>{formatPrice(stats.lowestValueAsset.price)}</Value>
+              </IndicatorColor>
+            )}
+          />
+          <StatCard 
+            disabled={isEmpty}
+            label="Rarest Asset" 
+            asset={stats.rarestAsset}
+            value={stats.rarestAsset && <Value>N/A</Value>} 
+          />
+          <StatCard 
+            disabled={isEmpty}
+            label="Highest All Time High" 
+            asset={stats.highestAllTimeHigh} 
+            value={(stats.highestAllTimeHigh &&
+              <IndicatorColor isPositive={true}>
+                <Value>{formatPrice(stats.highestAllTimeHigh.ath)}</Value>
+              </IndicatorColor>
+            )}
+          />
+        </StatsWrapper>
+        {/* </StatsOverflow> */}
       </Wrapper>
-    </Card>
+    </StyledCard>
   )
 }
