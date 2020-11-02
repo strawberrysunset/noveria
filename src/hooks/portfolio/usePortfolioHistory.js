a9588f0e6c3fa21137ef3fb36c873f2811b3a46b
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-02 15:01:20 +0000

    Remove history days option and initial data.

--- src/hooks/portfolio/usePortfolioHistory.js
+++ src/hooks/portfolio/usePortfolioHistory.js
@@ -1,21 +1,20 @@
-import React from 'react'
 import { getPortfolioHistory } from "./getPortfolioHistory"
 import {useSettings} from '../../context'
 import {usePortfolio} from '../../context'
 import {useQuery} from 'react-query'
 
-export const usePortfolioHistory = (initialHistoryDays) => {
-  const [historyDays, setHistoryDays] = React.useState(initialHistoryDays)
+export const usePortfolioHistory = () => {
+
   const {currency} = useSettings()
   const {assets} = usePortfolio()
 
-  const {data, ...asyncInfo} = useQuery(['portfolio-history', currency, assets, historyDays], async () => {
-    return await getPortfolioHistory({assets, currency, days: historyDays})
-  })
+  const {data, ...asyncInfo} = useQuery(['portfolio-history', currency, assets], async () => {
+    return await getPortfolioHistory({assets, currency, days: 30})
+  }, {initialData: []})
+
 
   return {
     history: data,
-    setHistoryDays,
     ...asyncInfo
   }
 }
