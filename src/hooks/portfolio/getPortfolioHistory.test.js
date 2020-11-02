0ccd86ab83a49ce1cb7c4e1d207545aaabd6bb92
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-02 14:44:22 +0000

    Delete files.

--- src/hooks/portfolio/getPortfolioHistory.test.js
+++
@@ -1,8 +0,0 @@
-import {sumHistories} from './getPortfolioHistory'
-
-const array1 = [[0, 1], [1, 2]]
-const array2 = [[1, 2], [3, 4]]
-const array3 = [array1, array2]
-test('Summation function works correctly', () => {
-  expect(sumHistories(array3)).toBe([[1, 3], [3, 6]])
-})
