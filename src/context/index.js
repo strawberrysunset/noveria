30c0b63976eb74ee503cde8bf95b9ae8b970840f
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-04 16:29:35 +0000

    Reduce import count and rename variables.

--- src/context/index.js
+++ src/context/index.js
@@ -1,42 +1,38 @@
 import React from 'react'
 import {IconContext} from 'react-icons'
 import {QueryCache, ReactQueryCacheProvider, ReactQueryConfigProvider} from 'react-query'
-import {useMenu as useMenuRaw} from '../hooks/menu'
-import {useNotification as useNotificationRaw} from '../hooks/notification'
-import {usePortfolio as usePortfolioRaw} from '../hooks/portfolio'
-import {useSettings as useSettingsRaw} from '../hooks/settings'
-import {useTheme as useThemeRaw} from '../hooks/theme'
+import {useMenu, useNotification, usePortfolio, useSettings, useTheme} from '../hooks'
 import {createContext} from './createContext'
 import {ThemeContext} from 'styled-components'
 
 // We create a context consumer and provider for each hook.
-const [useMenu, MenuProvider] = createContext({hook: useMenuRaw, name: 'Menu'})
-const [useNotification, NotificationProvider] = createContext({hook: useNotificationRaw, name: 'Notification'})
-const [useSettings, SettingsProvider] = createContext({hook: useSettingsRaw, name: 'Settings'})
+const [useMenuContext, MenuProvider] = createContext({hook: useMenu, name: 'Menu'})
+const [useNotificationContext, NotificationProvider] = createContext({hook: useNotification, name: 'Notification'})
+const [useSettingsContext, SettingsProvider] = createContext({hook: useSettings, name: 'Settings'})
 
-// Since some context values depend on parent provider state, we declare the hook inter dependencies before creating the child contexts.
-const usePortfolioModified = () => {
-  const {currency} = useSettings()
-  return usePortfolioRaw({currency})
+// Since some context values depend on parent provider state, we link up the hook dependencies before creating the child contexts.
+const usePortfolioWithDependency = () => {
+  const {currency} = useSettingsContext()
+  return usePortfolio({currency})
 }
 
-const useThemeModified = () => {
-  const {theme, darkMode} = useSettings()
-  return useThemeRaw({theme, darkMode})
+const useThemeWithDependency = () => {
+  const {theme, darkMode} = useSettingsContext()
+  return useTheme({theme, darkMode})
 }
 
-// Context interdependencies have been declared, now we create the remaining context.
-const [usePortfolio, PortfolioProvider] = createContext({hook: usePortfolioModified, name: 'Portfolio'})
-const [useTheme, ThemeProvider] = createContext({context: ThemeContext, hook: useThemeModified, name: 'Theme'})
+// Context dependencies have been declared, now we create the remaining context.
+const [usePortfolioContext, PortfolioProvider] = createContext({hook: usePortfolioWithDependency, name: 'Portfolio'})
+const [useThemeContext, ThemeProvider] = createContext({context: ThemeContext, hook: useThemeWithDependency, name: 'Theme'})
 
 // The benefit of this method is that context relations are clear.
 
 export {
-  useTheme,
-  usePortfolio,
-  useNotification,
-  useMenu,
-  useSettings
+  useThemeContext as useTheme,
+  usePortfolioContext as usePortfolio,
+  useSettingsContext as useSettings,
+  useNotificationContext as useNotification,
+  useMenuContext as useMenu,
 }
 
 const queryCache = new QueryCache()
