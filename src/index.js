1bc96311aa8b1534105a0deefa01989bd0cc287b
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-04 14:16:59 +0000

    Replace context providers with single provider.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { 
-  ThemeProvider,
-  SettingsProvider,
-  PortfolioProvider,
-  MenuProvider,
-  NotificationProvider
-} from './context'
-import {QueryCache, ReactQueryCacheProvider, ReactQueryConfigProvider} from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import {App} from './components'
-import {IconContext} from 'react-icons'
-
-const reactQueryConfig = {
-  queries: {
-    keepPreviousData: true,
-    initialStale: true
-  }
-}
-
-const queryCache = new QueryCache()
+import { ContextProvider } from './context'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <ReactQueryCacheProvider cache={queryCache}>
-          <ReactQueryConfigProvider config={reactQueryConfig}>
-            <SettingsProvider>
-              <NotificationProvider>
-                <PortfolioProvider>
-                  <MenuProvider>
-                    <ThemeProvider>
-                      <App/>
-                    </ThemeProvider>
-                  </MenuProvider>
-                </PortfolioProvider>
-              </NotificationProvider>
-            </SettingsProvider>
-          </ReactQueryConfigProvider>
-        </ReactQueryCacheProvider>
-      </IconContext.Provider>
+      <ContextProvider>
+        <App/>
+      </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
