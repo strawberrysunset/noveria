d1205a01de0b4a93253beb6c7dc2da1351be91bf
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-12 12:52:40 +0000

    Simplify context provider generator.

--- src/context/createContextAndProvider.js
+++ src/context/createContextAndProvider.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-// Create a context consumer and provider from a hook. Optionally override the default context value with your own. Optionally pass a name for more descriptive error messages.
-export const createContextAndProvider = ({context, hook, name}) => {
+// Create a context consumer and provider from a useHook. Optionally override the default context value with your own. Optionally pass a name for more descriptive error messages.
+export const createContextAndProvider = (useHook, context) => {
   
+  const name = useHook.name.slice(3)
   const Context = context || React.createContext()
   Context.displayName = name
   
   const ContextProvider = ({ children }) => {
-    const state = hook() // Initialize the hook in the provider.
+    const state = useHook() // Initialize the useHook in the provider.
     return (
       <Context.Provider value={state}>
         {children}
       </Context.Provider>
     )
   }
-  // Create a custom useContext hook.
+  // Create a custom useContext useHook.
   const useContext = () => {
     const context = React.useContext(Context)
     if (context === undefined) {
